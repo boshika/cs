ee23b4e5c872eeb969cb9b159f6d5d4055281880
boshikatara <boshikatara@gmail.com>
2020-06-03 19:04:58 -0700

    adds function todos

---
+++ python/miniprojects/tictactoe.py
@@ -0,0 +1,13 @@
+"""Tic Tac Toe"""
+
+def createBoard():
+    # TODO: create a tic tac toe board
+    board = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'X']
+    print()
+
+def startGame():
+    # TODO: ask players to choose
+    #  start the game
+
+def gameLogic():
+    # TODO: game logic goes here
