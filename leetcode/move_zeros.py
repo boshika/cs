938c80ff143aa41e3c3676d40e1fd0777ebb2bf2
boshikatara <boshikatara@gmail.com>
2020-10-29 09:22:47 -0700

    onepointer solution

--- leetcode/move_zeros.py
+++ leetcode/move_zeros.py
@@ -14,6 +14,11 @@ Time Complexity: O(n), Space:O(1)
 """
 
 def moveZeroes(nums):
+    """
+    Space is 99% better, but time is only 49% better. Reason could be
+    swapping the o and non zero values could be expensive. See Alternate
+    solution
+    """
     def moveZeroes(nums):
         """
         Do not return anything, modify nums in-place instead.
@@ -25,5 +30,27 @@ def moveZeroes(nums):
                 nums[i], nums[p1] = nums[p1], nums[i]
 
                 p1 += 1
+
+        return nums
+
+    def moveZeroes(nums):
+        """
+        This solution is 100% better in terms of space and 99.49% better in run time
+        Instead of swapping, I am just copying the non zero values to the index containing zeros
+        At the end, I am adding zeros from where the p2 index starts to the length of the list
+        :param nums:
+        :return:list
+        Time Complexity: O(n), Space: O(1)
+        """
+        p1 = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[p1] = nums[i]
+
+                p1 += 1
+
+        for j in range(p1, len(nums)):
+            nums[j] = 0
 
         return nums
