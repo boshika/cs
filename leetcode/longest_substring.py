b7f671b5f6b907ea0586a9b92b80799e5975a738
boshikatara <boshikatara@gmail.com>
2020-12-10 15:12:16 -0800

    longest substring

---
+++ leetcode/longest_substring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        p1 = 0
+        d = {}
+        curr_length, final_length = 0, 0
+
+        for idx, val in enumerate(s):
+            if val in d and d[val] >= p1:
+                p1 = 1 + d[val]
+                curr_length = idx - d[val]
+                d[val] = idx
+            else:
+                d[val] = idx
+                curr_length += 1
+                if curr_length > final_length:
+                    final_length = curr_length
+
+        return final_length
