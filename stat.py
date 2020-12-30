42680fd34f2cc919a4f24335ad39b2bf8efa3114
Boshika Tara <boshikatara@gmail.com>
2020-12-30 15:14:15 -0800

    Create stat.py

---
+++ stat.py
@@ -0,0 +1,57 @@
+'''Implement Central Tendency, Five number summary, Variance and STD
+'''
+class CentralTendency:
+  """
+  A class to represent Central Tendency for collections
+
+  ...
+
+  Attributes
+  ----------
+  lst: list
+     collection of numbers
+
+  Methods
+  -------
+  mean()
+     calculates and return the mean for a collection
+  median()
+     calculates the median for a collection
+  mode()
+     calculates the mode of a collection
+  """
+
+  def __init__(self, lst):
+    self.lst = lst
+    if len(self.lst) == 0:
+      raise ValueError (f'List cannot be empty')
+  
+  def mean(self):
+    return sum(self.lst)/len(self.lst)
+  
+  def median(self):
+    lst_length = len(self.lst) 
+    middle_index = lst_length//2
+
+    if lst_length%2==0:
+      return (self.lst[middle_index] + self.lst[middle_index-1])/2
+    else:
+      return self.lst[middle_index]
+  
+  def mode(self):
+    dict = {}
+    mode = []
+
+    for idx, value in enumerate(self.lst):
+      if value in dict:
+        dict[value] += 1
+      else:
+        dict[value] = 1
+    
+    max_freq = max(list(dict.values()))
+
+    for key, value in dict.items():
+      if value == max_freq:
+        mode.append(key)
+        
+    return mode
