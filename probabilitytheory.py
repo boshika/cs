e9980e1b85a3c55e809ba7bd8fd6e49d96392ae5
Boshika Tara <boshikatara@gmail.com>
2020-12-31 10:35:25 -0800

    Update probabilitytheory.py
    ran implementation to validate logic

--- probabilitytheory.py
+++ probabilitytheory.py
@@ -22,10 +22,39 @@ class SetTheory:
 
   def union(self):
     return set().union(*self.unpack_args)
-      
+ 
+
+
+# ----------------------------------------------
+# Implementation 
+# ----------------------------------------------
 A={'HHH','HHT','HTH','THH'}
 B={'HHH','HHT','THT','THH'}
 
 set_theory = SetTheory(A,B)
 print(set_theory)
 print(set_theory.union())
+
+def calculate_probability(n):
+  '''Calulate probability student attends atleast one courtse'''
+
+  stat = []
+  computer_science = []
+  data_science = []
+  for i in range(1,n+1):
+    if i%2==0:
+      stat.append(i)
+    if i%3==0:
+      computer_science.append(i)
+    if i>20:
+      data_science.append(i)
+      
+    stat = set(stat)
+    computer_science = set(computer_science)
+    data_science = set(data_science)
+
+    set_theory = SetTheory(stat, computer_science,data_science)
+    atleast_one_course = set_theory.union()
+    return atleast_one_course
+
+print(calculate_probability(30))
