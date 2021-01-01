72ba82dacf7ef9e97d8e1b71f434f9ff7ee694a1
Boshika Tara <boshikatara@gmail.com>
2021-01-01 15:57:17 -0800

    Create mymod.py
    modulo implmentation

---
+++ interviewchallenges/mymod.py
@@ -0,0 +1,13 @@
+def my_dulo(num1, num2):
+'''Implements the modulo operator'''
+    if num2 == 0:
+        return None
+    if num1 == 0 or num2 == 0 or type(num1) != int or type(num2) != int:
+        return None
+        
+    mod = num1-num2*(num1//num2)
+    
+    return mod
+
+print(my_dulo(2,0))
+print(my_dulo(0,2))
